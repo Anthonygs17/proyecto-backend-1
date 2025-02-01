c9abf5adaeb9cfc1b9d8ec24e4400ebaac4f1efa
Anthonygs17 <anthony.guimarey@utec.edu.pe>
2025-02-01 15:17:20 -0500

    rutas agregado

---
+++ src/routes/carts.router.js
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+import { CartManager } from '../managers/cartManager.js';
+
+const router = Router();
+const carts_path = 'src/data/carts.json';
+const cartManager = new CartManager(carts_path);
+
+router.get('/', (req, res) => {
+    res.json(cartManager.getCarts());
+});
+
+router.get('/:cid', (req, res) => {
+    const id = parseInt(req.params.cid);
+    const cart = cartManager.getCartById(id);
+    if(cart !== undefined){
+        res.json(cart.products);
+    }else{
+        res.status(404).json({ msg: 'id no existe' });
+    }
+});
+
+router.post('/', (req, res) => {
+    const { products } = req.body;
+    if(!products){
+        res.status(400).json({ msg: 'Faltan datos. Se debe pasar products' });
+        return;
+    }
+    const cart = { products };
+    cartManager.addCart(cart);
+    res.status(201).json(cart);
+});
+
+router.post('/:cid/product/:pid', (req, res) => {
+    const cartId = parseInt(req.params.cid);
+    const productId = parseInt(req.params.pid);
+    if(cartManager.getCartById(cartId) === undefined){
+        res.status(404).json({ msg: 'cart id no existe' });
+        return;
+    }
+    cartManager.addProductToCart(cartId, productId);
+    res.status(201).json({ msg: 'Producto agregado al carrito' });
+});
+
+export default router;
---
+++ src/routes/products.router.js
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import { ProductManager } from '../managers/productManager.js';
+
+const router = Router();
+const products_path = 'src/data/products.json';
+const productManager = new ProductManager(products_path);
+
+router.get('/', (req, res) => {
+    res.json(productManager.getProducts());
+});
+
+router.get('/:pid', (req, res) => {
+    const id = parseInt(req.params.pid);
+    const product = productManager.getProductById(id);
+    if(product !== undefined){
+        res.json(product);
+    }else{
+        res.status(404).json({ msg: 'id no existe' });
+    }
+});
+
+router.post('/', (req, res) => {
+    const { title, description, code, price, stock } = req.body;
+    if(!title || !description || !code || !price || !stock){
+        res.status(400).json({ msg: 'Faltan datos. Se debe pasar title, description, code, price y stock' });
+        return;
+    }
+    const product = { title, description, code, price, stock };
+    productManager.addProduct(product);
+    res.status(201).json(product);
+});
+
+router.put('/:pid', (req, res) => {
+    const id = parseInt(req.params.pid);
+    if(productManager.getProductById(id) === undefined){
+        res.status(404).json({ msg: 'id no existe' });
+        return;
+    }
+    const updatedProduct = req.body;
+    productManager.updateProduct(id, updatedProduct);
+    res.json(updatedProduct);
+});
+
+router.delete('/:pid', (req, res) => {
+    const id = parseInt(req.params.pid);
+    if(productManager.getProductById(id) === undefined){
+        res.status(404).json({ msg: 'id no existe, intente otra vez' });
+        return;
+    }
+    productManager.deleteProduct(id);
+    res.status(200).json({ msg: 'Producto eliminado' });
+});
+
+export default router;
